99e8f693ecd29335dfbbdb07a3e61bd9c22376fc
gangadhar15291 <gangadhar15291@gmail.com>
2024-05-16 23:30:27 +0530

    shall-script

---
+++ 03.variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello suresh, how are you"?
+echo "suresh:: hai ramesh i am fine how r u?"
+echo "ramesh:: i am fine to, how is u r work"
+echo "suresh:: not bad fine, think upgared to devops"
