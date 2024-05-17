97a2ac274e3233604ad00008ec44bbe36aa22b69
gangadhar15291 <gangadhar15291@gmail.com>
2024-05-17 22:32:52 +0530

    varible

---
+++ 04.variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+
+
+
+echo "$PERSON1:: Hello $PERSON2, how are you"?
+echo "$PERSON2:: hai $PERSON i am fine how r u?"
+echo "$PERSON1:: i am fine to, how is u r work"
+echo "$PERSON2:: not bad fine, think upgared to devops"
